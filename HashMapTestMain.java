2dbbc79c02190e82c380c25299188d931d2c7d1c
mestroedge <98157564+mestroedge@users.noreply.github.com>
2022-03-07 07:58:37 -0800

    uc2

---
+++ HashMapTestMain.java
@@ -0,0 +1,24 @@
+
+public class HashMapTestMain {
+
+	public static void main(String[] args) {
+
+		String string = "To be or not to be";
+		String[] words = string.toLowerCase().split(" ");
+
+		HashMap<String, Integer> hashMap = new HashMap<>();
+
+		for (int i = 0; i < words.length; i++) {
+
+			String word = words[i];
+			Integer value = hashMap.get(word);
+			if (value == null) {
+				value = 1;
+			} else {
+				value += 1;
+			}
+			hashMap.add(word, value);
+		}
+		System.out.println(hashMap);
+	}
+}
